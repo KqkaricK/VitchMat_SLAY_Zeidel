cd3b5168576d5fe89c9be8f1f7c9b6dacc16e775
KqkaricK <andrew73-42@yandex.ru>
2021-05-16 12:37:48 +0700

    Добавьте файлы проекта.

---
+++ VitchMat_SLAY/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿
+namespace VitchMat_SLAY
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.t_raz = new DarkUI.Controls.DarkTextBox();
+            this.darkLabel1 = new DarkUI.Controls.DarkLabel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.darkButton1 = new DarkUI.Controls.DarkButton();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.darkLabel2 = new DarkUI.Controls.DarkLabel();
+            this.darkButton2 = new DarkUI.Controls.DarkButton();
+            this.L = new System.Windows.Forms.DataGridView();
+            this.U = new System.Windows.Forms.DataGridView();
+            this.darkLabel3 = new DarkUI.Controls.DarkLabel();
+            this.darkLabel4 = new DarkUI.Controls.DarkLabel();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.darkLabel5 = new DarkUI.Controls.DarkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.L)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.U)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // t_raz
+            // 
+            this.t_raz.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(73)))), ((int)(((byte)(74)))));
+            this.t_raz.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.t_raz.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.t_raz.Location = new System.Drawing.Point(96, 16);
+            this.t_raz.Name = "t_raz";
+            this.t_raz.Size = new System.Drawing.Size(51, 20);
+            this.t_raz.TabIndex = 0;
+            this.t_raz.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.t_raz_KeyPress);
+            // 
+            // darkLabel1
+            // 
+            this.darkLabel1.AutoSize = true;
+            this.darkLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkLabel1.Location = new System.Drawing.Point(12, 18);
+            this.darkLabel1.Name = "darkLabel1";
+            this.darkLabel1.Size = new System.Drawing.Size(78, 13);
+            this.darkLabel1.TabIndex = 1;
+            this.darkLabel1.Text = "Размерность:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Location = new System.Drawing.Point(11, 41);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(474, 203);
+            this.dataGridView1.TabIndex = 49;
+            this.dataGridView1.EditingControlShowing += new System.Windows.Forms.DataGridViewEditingControlShowingEventHandler(this.dataGridView1_EditingControlShowing);
+            this.dataGridView1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.dataGridView1_KeyPress);
+            // 
+            // darkButton1
+            // 
+            this.darkButton1.Location = new System.Drawing.Point(391, 13);
+            this.darkButton1.Name = "darkButton1";
+            this.darkButton1.Padding = new System.Windows.Forms.Padding(5);
+            this.darkButton1.Size = new System.Drawing.Size(94, 23);
+            this.darkButton1.TabIndex = 50;
+            this.darkButton1.Text = "Создать СЛАУ";
+            this.darkButton1.Click += new System.EventHandler(this.darkButton1_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.ColumnHeadersVisible = false;
+            this.dataGridView2.Location = new System.Drawing.Point(541, 255);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.RowHeadersVisible = false;
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(73, 203);
+            this.dataGridView2.TabIndex = 51;
+            // 
+            // darkLabel2
+            // 
+            this.darkLabel2.AutoSize = true;
+            this.darkLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkLabel2.Location = new System.Drawing.Point(495, 260);
+            this.darkLabel2.Name = "darkLabel2";
+            this.darkLabel2.Size = new System.Drawing.Size(41, 13);
+            this.darkLabel2.TabIndex = 54;
+            this.darkLabel2.Text = "Корни:";
+            // 
+            // darkButton2
+            // 
+            this.darkButton2.Location = new System.Drawing.Point(541, 13);
+            this.darkButton2.Name = "darkButton2";
+            this.darkButton2.Padding = new System.Windows.Forms.Padding(5);
+            this.darkButton2.Size = new System.Drawing.Size(73, 23);
+            this.darkButton2.TabIndex = 55;
+            this.darkButton2.Text = "Метод LU";
+            this.darkButton2.Click += new System.EventHandler(this.darkButton2_Click_1);
+            // 
+            // L
+            // 
+            this.L.AllowUserToAddRows = false;
+            this.L.AllowUserToDeleteRows = false;
+            this.L.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.L.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.L.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.L.ColumnHeadersVisible = false;
+            this.L.Location = new System.Drawing.Point(33, 255);
+            this.L.Margin = new System.Windows.Forms.Padding(2);
+            this.L.Name = "L";
+            this.L.ReadOnly = true;
+            this.L.RowHeadersVisible = false;
+            this.L.RowHeadersWidth = 51;
+            this.L.RowTemplate.Height = 24;
+            this.L.Size = new System.Drawing.Size(203, 203);
+            this.L.TabIndex = 56;
+            this.L.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.dataGridView1_KeyPress);
+            // 
+            // U
+            // 
+            this.U.AllowUserToAddRows = false;
+            this.U.AllowUserToDeleteRows = false;
+            this.U.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.U.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.U.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.U.ColumnHeadersVisible = false;
+            this.U.Location = new System.Drawing.Point(282, 255);
+            this.U.Margin = new System.Windows.Forms.Padding(2);
+            this.U.Name = "U";
+            this.U.ReadOnly = true;
+            this.U.RowHeadersVisible = false;
+            this.U.RowHeadersWidth = 51;
+            this.U.RowTemplate.Height = 24;
+            this.U.Size = new System.Drawing.Size(203, 203);
+            this.U.TabIndex = 57;
+            this.U.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.dataGridView1_KeyPress);
+            // 
+            // darkLabel3
+            // 
+            this.darkLabel3.AutoSize = true;
+            this.darkLabel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkLabel3.Location = new System.Drawing.Point(12, 260);
+            this.darkLabel3.Name = "darkLabel3";
+            this.darkLabel3.Size = new System.Drawing.Size(16, 13);
+            this.darkLabel3.TabIndex = 58;
+            this.darkLabel3.Text = "L:";
+            // 
+            // darkLabel4
+            // 
+            this.darkLabel4.AutoSize = true;
+            this.darkLabel4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkLabel4.Location = new System.Drawing.Point(261, 260);
+            this.darkLabel4.Name = "darkLabel4";
+            this.darkLabel4.Size = new System.Drawing.Size(18, 13);
+            this.darkLabel4.TabIndex = 59;
+            this.darkLabel4.Text = "U:";
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.AllowUserToAddRows = false;
+            this.dataGridView3.AllowUserToDeleteRows = false;
+            this.dataGridView3.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView3.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.ColumnHeadersVisible = false;
+            this.dataGridView3.Location = new System.Drawing.Point(541, 41);
+            this.dataGridView3.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.ReadOnly = true;
+            this.dataGridView3.RowHeadersVisible = false;
+            this.dataGridView3.RowHeadersWidth = 51;
+            this.dataGridView3.RowTemplate.Height = 24;
+            this.dataGridView3.Size = new System.Drawing.Size(73, 203);
+            this.dataGridView3.TabIndex = 60;
+            // 
+            // darkLabel5
+            // 
+            this.darkLabel5.AutoSize = true;
+            this.darkLabel5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkLabel5.Location = new System.Drawing.Point(521, 41);
+            this.darkLabel5.Name = "darkLabel5";
+            this.darkLabel5.Size = new System.Drawing.Size(15, 13);
+            this.darkLabel5.TabIndex = 61;
+            this.darkLabel5.Text = "y:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 469);
+            this.Controls.Add(this.darkLabel5);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.darkLabel4);
+            this.Controls.Add(this.darkLabel3);
+            this.Controls.Add(this.U);
+            this.Controls.Add(this.L);
+            this.Controls.Add(this.darkButton2);
+            this.Controls.Add(this.darkLabel2);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.darkButton1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.darkLabel1);
+            this.Controls.Add(this.t_raz);
+            this.Name = "Form1";
+            this.Text = "Решение СЛАУ";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.L)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.U)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DarkUI.Controls.DarkTextBox t_raz;
+        private DarkUI.Controls.DarkLabel darkLabel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private DarkUI.Controls.DarkButton darkButton1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private DarkUI.Controls.DarkLabel darkLabel2;
+        private DarkUI.Controls.DarkButton darkButton2;
+        private System.Windows.Forms.DataGridView L;
+        private System.Windows.Forms.DataGridView U;
+        private DarkUI.Controls.DarkLabel darkLabel3;
+        private DarkUI.Controls.DarkLabel darkLabel4;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private DarkUI.Controls.DarkLabel darkLabel5;
+    }
+}
+
---
+++ VitchMat_SLAY/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DarkUI.Forms;
+
+namespace VitchMat_SLAY
+{
+    public partial class Form1 : DarkForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void Razmer()
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView2.Rows.Clear();
+            L.Rows.Clear();
+            U.Rows.Clear();
+            dataGridView1.RowCount = Convert.ToInt32(t_raz.Text);
+            dataGridView1.ColumnCount = Convert.ToInt32(t_raz.Text) + 1;
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                dataGridView1.Rows[i].Cells[dataGridView1.ColumnCount - 1].Style.BackColor = Color.LightGreen;
+                for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = 0;
+                }
+            }
+            dataGridView2.RowCount = dataGridView1.RowCount;
+            dataGridView3.RowCount = dataGridView1.RowCount;
+            L.RowCount = dataGridView1.RowCount;
+            L.ColumnCount = dataGridView1.RowCount;
+            U.RowCount = dataGridView1.RowCount;
+            U.ColumnCount = dataGridView1.RowCount;
+        }
+        double Slay(int i, int j)
+        {
+            return Convert.ToDouble(dataGridView1.Rows[i].Cells[j].Value.ToString());
+        }
+        private void darkButton1_Click(object sender, EventArgs e)
+        {
+            if (Chek() == false)
+            {
+                MessageBox.Show("Размерность должна быть больше 1 и меньше 11");
+            }
+            else
+            {
+                Razmer();
+            }
+        }
+
+        /*Ниже куча проверок*/
+
+        bool Chek()
+        {
+            if (t_raz.Text == "")
+            {
+                return false;
+            }
+            else if(Convert.ToInt32(t_raz.Text) < 2 || Convert.ToInt32(t_raz.Text) > 10)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        private void t_raz_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if ((e.KeyChar <= 47 || e.KeyChar >= 58) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void dataGridView1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if ((e.KeyChar <= 47 || e.KeyChar >= 58) && number != 8 && e.KeyChar != '-' && e.KeyChar != ',')
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void dataGridView1_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)
+        {
+            TextBox tb = (TextBox)e.Control;
+            tb.KeyPress += new KeyPressEventHandler(dataGridView1_KeyPress);
+        }
+
+        /*Куча проверок закончена*/
+
+        private void darkButton2_Click_1(object sender, EventArgs e)
+        {
+            for (int i = 0; i < dataGridView1.RowCount; i++) //+0
+            {
+                dataGridView2.Rows[i].Cells[0].Value = 0;
+                for (int j = 0; j < dataGridView1.ColumnCount - 1; j++)
+                {
+                    if (i < j)
+                    {
+                        L.Rows[i].Cells[j].Value = 0;
+                    }
+                    else if (i >= j)
+                    {
+                        U.Rows[i].Cells[j].Value = 0;
+                    }
+                }
+            }
+            for (int i = 0; i < dataGridView1.RowCount; i++)  //Вычисление L и U
+            {
+                for (int k = i; k < dataGridView1.RowCount; k++)
+                {
+                    double S = 0;
+                    for (int j = 0; j < i; j++)
+                    {
+                        S += (Convert.ToDouble(L.Rows[i].Cells[j].Value.ToString()) * Convert.ToDouble(U.Rows[j].Cells[k].Value.ToString()));
+                    }
+                    U.Rows[i].Cells[k].Value = Slay(i, k) - S;
+                }
+                for (int k = i; k < dataGridView1.RowCount; k++)
+                {
+                    if (i == k)
+                    {
+                        L.Rows[i].Cells[i].Value = 1;
+                    }
+                    else
+                    {
+                        double S = 0;
+                        for (int j = 0; j < i; j++)
+                        {
+                            S += (Convert.ToDouble(L.Rows[k].Cells[j].Value.ToString()) * Convert.ToDouble(U.Rows[j].Cells[i].Value.ToString()));
+                        }
+                        L.Rows[k].Cells[i].Value = (Slay(k, i) - S) / Convert.ToDouble(U.Rows[i].Cells[i].Value.ToString());
+                    }
+                }
+            }
+            for (int i = 0; i < dataGridView1.RowCount; i++) // решение Ly = B
+            {
+                double S = 0;
+                for (int k = 0; k < i; k++)
+                {
+                    S += Convert.ToDouble(L.Rows[i].Cells[k].Value.ToString()) * Convert.ToDouble(dataGridView3.Rows[k].Cells[0].Value.ToString());
+                }
+                dataGridView3.Rows[i].Cells[0].Value = Convert.ToDouble(dataGridView1.Rows[i].Cells[dataGridView1.ColumnCount - 1].Value.ToString()) - S;
+            }
+            for (int i = dataGridView1.RowCount - 1; i >= 0; i--) // решение Ux = y
+            {
+                double S = 0;
+                for (int k = dataGridView1.RowCount - 1; k > 0; k--)
+                {
+                    S += Convert.ToDouble(U.Rows[i].Cells[k].Value.ToString()) * Convert.ToDouble(dataGridView2.Rows[k].Cells[0].Value.ToString());
+                }
+                dataGridView2.Rows[i].Cells[0].Value = (Convert.ToDouble(dataGridView3.Rows[i].Cells[0].Value.ToString()) - S) / Convert.ToDouble(U.Rows[i].Cells[i].Value.ToString());
+            }
+        }
+    }
+}
